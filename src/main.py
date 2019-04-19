dee8c7a681caffead922d5f1f4729be320024074
wangyurzee7 <wangyz17@tsinghua.org.cn>
2019-04-20 00:51:08 +0800

    main

---
+++ src/main.py
@@ -0,0 +1,39 @@
+from hmmpinyin import *
+import sys
+import os
+from utils import *
+
+
+ifile=None
+ofile=None
+def __input():
+    if ifile:
+        ret=ifile.readline()
+    else:
+        ret=input()
+    return ret.replace('\r','').replace('\n','')
+
+def __output(st):
+    if ofile:
+        ofile.write(st+'\n')
+    else:
+        print(st)
+
+if __name__=="__main__":
+    if len(sys.argv)>1:
+        ifile=sys.argv[1]
+        ensure_file_exists(ifile)
+        ifile=open(ifile,"r")
+    if len(sys.argv)>2:
+        ofile=open(sys.argv[2])
+
+    py=HmmPinyin("../data/model/mat.json","../data/model/maps.json")
+    st=__input()
+    while st:
+        __output(py.predict(st))
+        st=__input()
+
+    if ifile:
+        ifile.close()
+    if ofile:
+        ofile.close()
